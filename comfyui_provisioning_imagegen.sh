2cd5901a8a762192afb8a27e67516bd807b796af
Peter Kyme <peter.kyme@gmail.com>
2025-07-20 15:33:54 +0100

    Added flux kontext dev.

--- comfyui_provisioning_imagegen.sh
+++ comfyui_provisioning_imagegen.sh
@@ -252,6 +252,7 @@ download_models() {
         # ["https://huggingface.co/google/siglip-so400m-patch14-384/resolve/main/model.safetensors"]="clip_vision"
         # ["https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter_sdxl.safetensors"]="ipadapter"
         # ["https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/image_encoder/model.safetensors"]="clip_vision"
+        ["https://huggingface.co/QuantStack/FLUX.1-Kontext-dev-GGUF/resolve/main/flux1-kontext-dev-Q8_0.gguf"]="unet"
     )
     
     for url in "${!model_downloads[@]}"; do
